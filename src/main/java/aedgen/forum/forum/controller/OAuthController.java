bf862f9b86fa003482709b487665402e1cfcae55
zIDAedgen <sdkfz233@icloud.com>
2019-12-25 11:06:01 +0900

    25Dec

--- src/main/java/aedgen/forum/forum/controller/OAuthController.java
+++ src/main/java/aedgen/forum/forum/controller/OAuthController.java
@@ -5,6 +5,7 @@ import aedgen.forum.forum.DTO.AccessTokenDTO;
 import aedgen.forum.forum.DTO.GithubUser;
 import aedgen.forum.forum.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,21 @@ public class OAuthController {
 
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("f3f624b867637a65cede");
-        accessTokenDTO.setClient_secret("8850416daebb1fe19c497a9117b16732992fee92");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8885/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
