661fc23c8c7fff0ced1f0b722075f65df6ebf109
zIDAedgen <sdkfz233@icloud.com>
2020-01-06 07:39:10 +0900

    6Jan

---
+++ src/main/java/aedgen/forum/forum/controller/PublishController.java
@@ -0,0 +1,12 @@
+package aedgen.forum.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublishController {
+    @GetMapping("/publish")
+    public String publish() {
+        return "publish";
+    }
+}
